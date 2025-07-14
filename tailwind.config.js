c77a0407af321cb8f52d71908f2d0a11f2720a5a
MuhammadUmar <umer62490@gmail.com>
2025-07-14 21:40:24 +0500

    farmhouse design initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Important for CRA
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
